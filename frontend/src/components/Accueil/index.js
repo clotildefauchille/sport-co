ce2ebb256e82ca1f6eddb7da92c75b6f5a52a88a
jeremy-mairey <jeremymai.nemo@gmail.com>
2021-03-02 14:04:12 +0100

    premier test accueil

---
+++ frontend/src/components/Accueil/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Accueil = () => (
+  <div className="app">
+    <h1>Composant : App</h1>
+  </div>
+);
+
+export default Accueil;
--- frontend/src/components/App/index.js
+++ frontend/src/components/App/index.js
@@ -3,17 +3,14 @@ import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 
 // == Import
-import reactLogo from './react-logo.svg';
+import Accueil from 'src/components/Accueil';
 import './styles.css';
 
 // == Composant
 const App = () => (
   <Switch>
     <Route path="/" exact>
-      <div className="app">
-        <img src={reactLogo} alt="react logo" />
-        <h1>Composant : App</h1>
-      </div>
+      <Accueil />
     </Route>
   </Switch>
 );
