8d729c274b7756c01153eabe278de0b8a444d369
jeremy-mairey <jeremymai.nemo@gmail.com>
2021-03-05 16:24:04 +0100

    request one user to database and compare to data from front

--- backend/app/controllers/connectionController.js
+++ backend/app/controllers/connectionController.js
@@ -1,14 +1,36 @@
+const { User } = require("../models");
 
+const Sequelize = require("sequelize");
+const sequelize = require("../database.js");
 
 const connectionController = {
     getUser: async (req, res) => {
-        console.log(req.body);
-        res.json({
-            "firsname": "ton prénom",
-            "lastname": "ton nom",
-            "pseudo": "ton pseudo"
+        const data = req.body;
+        console.log(data.email); 
+        const result = await User.findOne({
+            where: {
+                email: data.email,
+            },
         });
-    }
-}
+        console.log(result);
+
+        // Test pour voir si l'email est valide
+        if (result === null) { 
+            res.status(404).json("Votre adresse mail n'est pas valide");
+        } 
+        // Test pour voir si le mot de passe est correct
+        else if(result.password != data.password) {        
+            res.status(404).json("Le mot de passe est incorrect");
+        } 
+        else {
+            res.json(
+            {
+                "firsname": `${result.firstname}`,
+                "lastname": `${result.lastname}`,
+                "pseudo": `${result.pseudo}`
+            }
+        )}
+    },
+};
 
 module.exports = connectionController;
