f4da65069efaa766dec2e588426ed4a5db791ed2
benjaminrusso-oclock <russobenjamin45@gmail.com>
2021-03-09 11:10:03 +0100

    init

---
+++ frontend/src/components/Registration/index.js
@@ -0,0 +1,18 @@
+// == Import npm
+import React from 'react';
+import { Link, Redirect } from 'react-router-dom';
+import PropTypes from 'prop-types';
+
+// == Import
+import './style.scss';
+
+// == Composant
+const Registration = () => {(
+        <section className="registration">
+        </section>
+)};
+
+Registration.propTypes = {};
+
+// == Export
+export default Registration;
