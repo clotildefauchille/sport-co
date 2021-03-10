00b752bda9e46c274c92c493231f8a6aa41faec6
benjaminrusso-oclock <russobenjamin45@gmail.com>
2021-03-10 15:15:27 +0100

    Add positionStack to find latitude & longitude user_place and send data to server

--- frontend/src/middlewares/registration.js
+++ frontend/src/middlewares/registration.js
@@ -7,49 +7,65 @@ import {
   resetErrors,
 } from 'src/actions/registration';
 
+const apiKey = '82a0b22e81932aad65c97e8bcc2f192a';
+
 const registration = (store) => (next) => (action) => {
   switch (action.type) {
     case FETCH_REGISTRATION_FORM: {
       const formData = store.getState().registration;
       console.log('registration store', formData);
       store.dispatch(resetErrors());
-
+      console.log(
+        `http://api.positionstack.com/v1/forward?access_key=${apiKey}&country=FR&query=${formData.address}, ${formData.postalCode} ${formData.city}`,
+      );
       if (formData.password !== formData.confirmPassword) {
         store.dispatch(passwordError());
         break;
-      }
-      else {
+      } else {
         axios
-          .post(`${process.env.API_URL}/api/registration`, {
-            pseudo: formData.pseudo,
-            email: formData.email,
-            password: formData.password,
-            confirmPassword: formData.confirmPassword,
-            firstname: formData.firstname,
-            lastname: formData.lastname,
-            place: {
-              city: formData.city,
-              number: '75',
-              street: 'Rue Simone Veil',
-              postal_code: '34470',
-              region: 'Hérault',
-              latitude: 43.574837,
-              longitude: 3.936389,
-            },
-            presentation: formData.presentation,
-          })
+          .get(
+            `http://api.positionstack.com/v1/forward?access_key=${apiKey}&country=FR&query=${formData.address},${formData.postalCode},${formData.city}`,
+          )
           .then((response) => {
-            console.log('response', response.data);
+            const responsePlace = response.data.data[0];
+            console.log('response', response.data.data[0]);
+
+            axios
+              .post(`${process.env.API_URL}/api/registration`, {
+                pseudo: formData.pseudo,
+                email: formData.email,
+                password: formData.password,
+                confirmPassword: formData.confirmPassword,
+                firstname: formData.firstname,
+                lastname: formData.lastname,
+                place: {
+                  city: responsePlace.city,
+                  number: responsePlace.number,
+                  street: responsePlace.street,
+                  postal_code: responsePlace.postal_code,
+                  region: responsePlace.region,
+                  latitude: responsePlace.latitude,
+                  longitude: responsePlace.longitude,
+                },
+                presentation: formData.presentation,
+              })
+              .then((APIresponse) => {
+                console.log('response', APIresponse.data);
+              })
+              .catch((error) => {
+                console.log('error', error.response.data);
+                if (error.response.data.error === 'mail') {
+                  store.dispatch(emailError());
+                }
+                else if (error.response.data.error === 'pseudo') {
+                  store.dispatch(pseudoError());
+                }
+              });
           })
           .catch((error) => {
             console.log('error', error.response.data);
-            if (error.response.data.error === 'mail') {
-              store.dispatch(emailError());
-            }
-            else if (error.response.data.error === 'pseudo') {
-              store.dispatch(pseudoError());
-            }
           });
+
         break;
       }
     }
