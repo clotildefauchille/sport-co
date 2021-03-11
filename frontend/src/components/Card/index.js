a17946f89892cf6c6a97b489863e4f59e098e83f
benjaminrusso-oclock <russobenjamin45@gmail.com>
2021-03-11 11:05:41 +0100

    Add link to card details in card & modalbox when disconnected

--- frontend/src/components/Card/index.js
+++ frontend/src/components/Card/index.js
@@ -1,6 +1,7 @@
 // == Import npm
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
 // == Import
 import './style.scss';
@@ -13,46 +14,79 @@ import sports from './sports';
 
 // == Composant
 const Card = ({ card, isLogged, showLoginModal }) => {
-  // console.log('CARD', card);
-  return (
-    <article className="card">
-      <a href="#" className="card__link">
-        <img src={sports[card.sport.name]} alt="" className="card__image" />
-        <h2 className="card__title">{card.title}</h2>
-        <div className="card__infos">
-          <div className="card__container">
-            <img src={clock} alt="" className="card__icon" />
-            <p className="card__text">
-              {card.time} - {card.date}
-            </p>
-          </div>
-          <div className="card__container">
-            <img src={pin} alt="" className="card__icon" />
-            <p className="card__text">
-              {card.activity_place.city}
-              {card.activity_place.distance && (
-                <>
-                  <br />({parseFloat(card.activity_place.distance).toFixed(1)} km)
-                </>
-              )}
-            </p>
-          </div>
-        </div>
-        <p className="card__description">{card.description}</p>
-      </a>
-
-      {isLogged ? (
-        <button className="card__join" type="button">
-          Rejoindre
-        </button>
-      ) : (
-        <button onClick={showLoginModal} className="card__join" type="button">
-          Rejoindre
-        </button>
-      )}
-      
-    </article>
-  );
+  const urlPath = `/activity/${card.id}`;
+  switch (isLogged) {
+    case true:
+      return (
+        <article className="card">
+          <Link to={urlPath} className="card__link">
+            <img src={sports[card.sport.name]} alt="" className="card__image" />
+            <h2 className="card__title">{card.title}</h2>
+            <div className="card__infos">
+              <div className="card__container">
+                <img src={clock} alt="" className="card__icon" />
+                <p className="card__text">
+                  {card.time} - {card.date}
+                </p>
+              </div>
+              <div className="card__container">
+                <img src={pin} alt="" className="card__icon" />
+                <p className="card__text">
+                  {card.activity_place.city}
+                  {card.activity_place.distance && (
+                    <>
+                      <br />(
+                      {parseFloat(card.activity_place.distance).toFixed(1)} km)
+                    </>
+                  )}
+                </p>
+              </div>
+            </div>
+            <p className="card__description">{card.description}</p>
+          </Link>
+          <button className="card__join" type="button">
+            Rejoindre
+          </button>
+        </article>
+      );
+    default:
+      return (
+        <article className="card">
+          <button type="button" onClick={showLoginModal} className="card__link">
+            <img src={sports[card.sport.name]} alt="" className="card__image" />
+            <h2 className="card__title">{card.title}</h2>
+            <div className="card__infos">
+              <div className="card__container">
+                <img src={clock} alt="" className="card__icon" />
+                <p className="card__text">
+                  {card.time} - {card.date}
+                </p>
+              </div>
+              <div className="card__container">
+                <img src={pin} alt="" className="card__icon" />
+                <p className="card__text">
+                  {card.activity_place.city}
+                  {card.activity_place.distance && (
+                    <>
+                      <br />(
+                      {parseFloat(card.activity_place.distance).toFixed(1)} km)
+                    </>
+                  )}
+                </p>
+              </div>
+            </div>
+            <p className="card__description">{card.description}</p>
+          </button>
+          <button
+            onClick={showLoginModal}
+            className="card__join"
+            type="button"
+          >
+            Rejoindre
+          </button>
+        </article>
+      );
+  }
 };
 
 Card.propTypes = {
