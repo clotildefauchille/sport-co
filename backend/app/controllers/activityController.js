dd3ea26573c497d4a24dc94998433324e27807f1
boris-couderc <boris-couderc@users.noreply.github.com>
2021-03-11 13:52:15 +0100

    fix bug controller activityController

--- backend/app/controllers/activityController.js
+++ backend/app/controllers/activityController.js
@@ -58,6 +58,40 @@ const activityController = {
     }
 
     try {
+      /**
+       * Formule mathématique pour le calcul de distance entre 2 points A et B sur terre avec lat et lng,
+       * 6371 est le rayon de la terre en km :
+       *
+       * x = (lngB - lngA) * cos((latA + latB)/2)
+       * y = latA - latB
+       * distance = sqrt(pow(x) + pow(y)) * 6371
+       *
+       * mais /!\ il faut convertir et remplacer les lat et lng en radian :
+       * latInRad = lat * pi() / 180
+       * lngInRad = lng * pi() / 180
+       *
+       * besoin d'utiliser une sous requete avec alias pour calcul et recup la distance :
+       * https://sql.sh/cours/sous-requete
+       *
+       * ADAPTATION SQL :
+       */
+
+      const distanceCalcul = sequelize.literal(`(
+      SELECT (
+        SQRT(
+          POW (
+            (("lng" * pi() / 180) - ( ${lng} * pi() / 180))
+              * COS((( "lat" * pi() / 180) + ( ${lat} * pi() / 180)) / 2), 2
+            ) 
+            + POW (
+              ("lat" * pi() / 180) - ( ${lat} * pi() / 180), 2
+            )
+          ) * 6371
+        )
+        FROM activity_place
+        WHERE activity_place_id = activity_place.id
+      )`);
+
       const activities = await Activity.findAll({
         where: {
           activity_status_id: 3,
@@ -69,7 +103,7 @@ const activityController = {
           {
             association: 'activity_place',
             attributes: {
-              include: [[sequelize.literal(distanceCalculSQL(lat, lng)), 'distance']],
+              include: [[distanceCalcul, 'distance']],
             },
           },
         ],
