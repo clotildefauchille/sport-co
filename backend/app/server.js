504a0934fdf3aa05b253622649ea5938c68bcb62
boris-couderc <boris-couderc@users.noreply.github.com>
2021-03-12 20:16:50 +0100

    init

--- backend/app/server.js
+++ backend/app/server.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const router = require('./router');
-const jwt = require('express-jwt');
-const jsonwebtoken = require('jsonwebtoken');
+//const jwt = require('express-jwt');
+//const jsonwebtoken = require('jsonwebtoken');
 const cors = require("cors");
 
-const jwtSecret = process.env.JWT_SECRET;
+//const jwtSecret = process.env.JWT_SECRET;
 
 const PORT = process.env.PORT || 5000;
 
@@ -15,15 +15,14 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+/*
 app.get('/jwt', (req, res) => {
     res.json({
       token: jsonwebtoken.sign({ user: 'johndoe' }, jwtSecret)
     });
 });
-
+*/
 //app.use(jwt({ secret: jwtSecret, algorithms: ['HS256'] }));
-
-
 
 
 app.use(router);
