fe6f10e2249df62a136a8ae55416d948c4126c3d
boris-couderc <boris-couderc@users.noreply.github.com>
2021-03-14 19:21:17 +0100

    add where gte now in sequelize queries activities

--- backend/app/controllers/activityController.js
+++ backend/app/controllers/activityController.js
@@ -21,9 +21,6 @@ const activityController = {
     }
     try {
       const activities = await Activity.findAll({
-        where: {
-          activity_status_id: 3,
-        },
         attributes: { 
           exclude: ['activity_status_id','activity_place_id','sport_id','creator_id'] 
         },
@@ -47,6 +44,18 @@ const activityController = {
             attributes: ['pseudo']
           },
         ],
+        where: {
+          [Op.and]: [
+            {
+              activity_status_id: 3,
+            },
+            {
+              date: {
+                [Op.gte]: sequelize.fn('NOW'),
+              }
+            }
+          ]
+        },
         offset: (page - 1) * activityController.defaultNumCardInPage,
         limit: activityController.defaultNumCardInPage,
         order: [['created_at', 'DESC']],
@@ -179,6 +188,11 @@ const activityController = {
             ),
             {
               activity_status_id: 3,
+            },
+            {
+              date: {
+                [Op.gte]: sequelize.fn('NOW'),
+              }
             }
           ]
         },
@@ -259,6 +273,11 @@ const activityController = {
               sport_id: {
                 [Op.or]: sports
               }
+            },
+            {
+              date: {
+                [Op.gte]: sequelize.fn('NOW'),
+              }
             }
           ]
         },
@@ -317,6 +336,11 @@ const activityController = {
             }
           },
         ],
+        where: {
+          date: {
+            [Op.gte]: sequelize.fn('NOW'),
+          }
+        },
         offset: (page - 1) * activityController.defaultNumCardInPage,
         limit: activityController.defaultNumCardInPage,
         order: [['date', 'ASC']],
