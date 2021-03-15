6556caf50d46e5f850cc6521cf116768f252c2a4
benjaminrusso-oclock <russobenjamin45@gmail.com>
2021-03-15 09:23:30 +0100

    Add map on Activity Details done

--- frontend/src/components/Details/index.js
+++ frontend/src/components/Details/index.js
@@ -3,10 +3,19 @@ import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { useParams } from 'react-router-dom';
 
+import ReactMapboxGl, { Marker } from 'react-mapbox-gl';
+import 'mapbox-gl/dist/mapbox-gl.css';
+
 // == Import
 import './style.scss';
 import userIcon from 'src/assets/icons/account_circle.svg';
+import pin from 'src/assets/icons/pin.svg';
 import sports from 'src/components/Card/sports';
+
+const Map = ReactMapboxGl({
+  accessToken:
+    'pk.eyJ1IjoicnVzc29iZW5qYW1pbiIsImEiOiJja205OGxjNngxZm52MnFqeGk1MmwyajJrIn0.M7PtoR3vltTPsc7q-4QDkg',
+});
 
 const Details = ({ activity, fetchDataActivity, onClickJoin }) => {
   let currentSport = '';
@@ -22,64 +31,93 @@ const Details = ({ activity, fetchDataActivity, onClickJoin }) => {
   }
 
   return (
-    <main
-      className="activity"
-      style={{
-        backgroundImage: `url(${currentSport})`,
-      }}
-    >
+    <>
+      <main
+        className="activity"
+        style={{
+          backgroundImage: `url(${currentSport})`,
+        }}
+      >
+        {activity.id && (
+          <>
+            <section className="activity__header">
+              <h1 className="activity__title">{activity.title}</h1>
+              <p className="activity__timeplace">
+                Le {activity.date} à {activity.time} à{' '}
+                {activity.activity_place.city}
+              </p>
+              <p className="activity__duration">Durée : {activity.duration}</p>
+              <button
+                type="button"
+                className={
+                  'activity__join activity__join-' + activity.classname
+                }
+                onClick={onClickJoin}
+              >
+                {activity.message}
+              </button>
+            </section>
+            <section className="activity__details">
+              <div className="activity__description">
+                <h2 className="activity__descriptionTitle">Description :</h2>
+                <p>{activity.description}</p>
+              </div>
+              <div className="activity__creator">
+                <img
+                  src={userIcon}
+                  className="activity__userIcon"
+                  alt="user icon"
+                />
+                <div className="activity__container">
+                  <p className="activity__pseudo">{activity.creator.pseudo}</p>
+                  <p className="activity__role">Créateur</p>
+                </div>
+              </div>
+              <div className="activity__participants">
+                <img
+                  src={userIcon}
+                  className="activity__participantsIcon"
+                  alt="participants icon"
+                />
+                <div className="activity__container">
+                  <p className="activity__participantsNumber">
+                    {activity.participant_count} participant(s)
+                  </p>
+                  <p className="activity__minParticipants">
+                    Participants minimum : {activity.min_participant}
+                  </p>
+                </div>
+              </div>
+            </section>
+          </>
+        )}
+      </main>
       {activity.id && (
-        <>
-          <section className="activity__header">
-            <h1 className="activity__title">{activity.title}</h1>
-            <p className="activity__timeplace">
-              Le {activity.date} à {activity.time} à{' '}
-              {activity.activity_place.city}
-            </p>
-            <p className="activity__duration">Durée : {activity.duration}</p>
-            <button
-              type="button"
-              className={'activity__join activity__join-' + activity.classname}
-              onClick={onClickJoin}
+        <Map
+          center={[activity.activity_place.lng, activity.activity_place.lat]}
+          zoom={[17]}
+          style="mapbox://styles/mapbox/streets-v11"
+          className="activity__map"
+        >
+          <Marker
+            coordinates={[
+              activity.activity_place.lng,
+              activity.activity_place.lat,
+            ]}
+            anchor="bottom"
+          >
+            <img src={pin} alt="pin on map" className="activity__pin" />
+          </Marker>
+          {/* <Layer
+              type="symbol"
+              layout={{ 'icon-image': 'harbor-15' }}
+              wrapped
             >
-              {activity.message}
-            </button>
-          </section>
-          <section className="activity__details">
-            <div className="activity__description">
-              <h2 className="activity__descriptionTitle">Description :</h2>
-              <p>{activity.description}</p>
-            </div>
-            <div className="activity__creator">
-              <img
-                src={userIcon}
-                className="activity__userIcon"
-                alt="user icon"
-              />
-              <div className="activity__container">
-                <p className="activity__pseudo">{activity.creator.pseudo}</p>
-                <p className="activity__role">Créateur</p>
-              </div>
-            </div>
-            <div className="activity__participants">
-              <img
-                src={userIcon}
-                className="activity__participantsIcon"
-                alt="participants icon"
-              />
-              <div className="activity__container">
-                <p className="activity__participantsNumber">
-                  {activity.participant_count} participant(s)
-                </p>
-                <p className="activity__minParticipants">
-                  Participants minimum : {activity.min_participant}
-                </p>
-              </div>
-            </div>
-          </section>
-        </>
+              <Feature coordinates={[activity.activity_place.lng, activity.activity_place.lat]} />
+            </Layer> */}
+        </Map>
       )}
-    </main>
+    </>
   );
 };
 
